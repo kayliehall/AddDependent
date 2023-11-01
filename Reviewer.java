3d3b4188ef128346cab1b77fbe8c91ff44e2830a
Shisan7 <72416635+Shisan7@users.noreply.github.com>
2023-11-01 12:56:25 -0400

    Comment
    Comment on reviewer.java

--- Reviewer.java
+++ Reviewer.java
@@ -2,5 +2,5 @@ public class Reviewer {
     WorkFlow workFlow = new WorkFlow();
     DependentAdd getApplication(){
         return workFlow.getApplication();
-    }
+    }//Just some modifications.
 }
